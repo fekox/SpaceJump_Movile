acb120394fb82fd161b32848f64b4614e917ee27
fekox <facusantos123@gmail.com>
2023-10-10 21:46:51 -0300

    Now the plugin is copied to the project's plugins folder

--- SantosPlugin/SantosLoggerPlugin/build.gradle.kts
+++ SantosPlugin/SantosLoggerPlugin/build.gradle.kts
@@ -36,3 +36,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+tasks.register<Copy>("copyBuild")
+{
+    from("build/outputs/aar")
+    include(project.name + "-release.aar")
+    into("../../Assets/Plugins/Android")
+}
