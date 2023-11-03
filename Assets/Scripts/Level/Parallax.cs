b1f9e2a000527fa7c84efdeb388c7fa74e67489c
fekox <facusantos123@gmail.com>
2023-11-02 21:10:36 -0300

    Add parallax background

--- Assets/Scripts/Level/Parallax.cs
+++ Assets/Scripts/Level/Parallax.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private bool scrollLeft;
+
+    private float textureWidth;
     void Start()
     {
-        
+        SetupTexture();
+
+        if (scrollLeft) 
+        {
+            moveSpeed = -moveSpeed;
+        }
     }
 
-    // Update is called once per frame
+    private void SetupTexture() 
+    {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        textureWidth = sprite.texture.width / sprite.pixelsPerUnit;
+    }
+
+    private void Scroll() 
+    {
+        float delta = moveSpeed * Time.deltaTime;
+        transform.position += new Vector3(delta, 0f, 0f);
+    }
+
+    private void CheckReset() 
+    {
+        if ((Mathf.Abs(transform.position.x) - textureWidth) > 0) 
+        {
+            transform.position = new Vector3(0.0f, transform.position.y, transform.position.z);
+        }
+    }
+
     void Update()
     {
-        
+        Scroll();
+        CheckReset();
     }
 }
