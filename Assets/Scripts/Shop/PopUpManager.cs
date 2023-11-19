cdec336e2b95acffdafcffa69127e40e42840a5e
fekox <facusantos123@gmail.com>
2023-11-19 13:39:31 -0300

    Add two powerUp on the shop

--- Assets/Scripts/Shop/PopUpManager.cs
+++ Assets/Scripts/Shop/PopUpManager.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 public class PopUpManager : MonoBehaviour
 {
     [SerializeField] private Image skinImage;
+    [SerializeField] private GameObject skinImageGo;
     [SerializeField] private TextMeshProUGUI priceText;
 
     public void SetSkinImage(Image newSkinImage) 
     {
+        skinImageGo.SetActive(true);
         skinImage.sprite = newSkinImage.sprite;
     }
 
     public void SetPriceText(TextMeshProUGUI newPriceText) 
     {
+        skinImageGo.SetActive(true);
         priceText.text = newPriceText.text;
+    }
+
+    public void CheckPowerUp() 
+    {
+        skinImageGo.SetActive(false);
     }
 }
--- Assets/Scripts/UI/ShopMenuButtons.cs
+++ Assets/Scripts/UI/ShopMenuButtons.cs
@@ -7,20 +7,22 @@ using UnityEngine.UI;
 
 public class ShopMenuButtons : MonoBehaviour
 {
+    [Header("Shop References")]
     [SerializeField] private string[] sceneName;
-    [SerializeField] private GameObject shop;
-    [SerializeField] private GameObject popUp;
     [SerializeField] private PopUpManager popUpManager;
     [SerializeField] private Image[] skinImage;
+    [SerializeField] private GameObject shop;
+    [SerializeField] private GameObject popUp;
+    [SerializeField] private GameObject menuButton;
 
+    [Header("PowerUps texts")]
+    [SerializeField] private GameObject X2Text;
+    [SerializeField] private GameObject plus50MText;
+
+    [Header("PriceText")]
     [SerializeField] private TextMeshProUGUI priceText;
-    private string priceTextValue = "5";
-
-    private void Start()
-    {
-        priceText.text = priceTextValue;
-    }
-
+    private string priceSkinTextValue = "5";
+    private string pricePowerUpTextValue = "10";
     public void ReturnMenu()
     {
         SceneManager.LoadScene(sceneName[0]);
@@ -28,24 +30,58 @@ public class ShopMenuButtons : MonoBehaviour
 
     public void ShowPinkManSkin() 
     {
+        priceText.text = priceSkinTextValue;
         popUpManager.SetSkinImage(skinImage[0]);
         popUpManager.SetPriceText(priceText);
+        X2Text.SetActive(false);
+        plus50MText.SetActive(false);
+        menuButton.SetActive(false);
         shop.SetActive(false);
         popUp.SetActive(true);
     }
 
     public void ShowNinjaFrogSkin() 
     {
+        priceText.text = priceSkinTextValue;
         popUpManager.SetSkinImage(skinImage[1]);
         popUpManager.SetPriceText(priceText);
+        X2Text.SetActive(false);
+        plus50MText.SetActive(false);
+        menuButton.SetActive(false);
         shop.SetActive(false);
         popUp.SetActive(true);
     }
 
     public void ShowMaskDudeSkin() 
     {
+        priceText.text = priceSkinTextValue;
         popUpManager.SetSkinImage(skinImage[2]);
         popUpManager.SetPriceText(priceText);
+        X2Text.SetActive(false);
+        plus50MText.SetActive(false);
+        menuButton.SetActive(false);
+        shop.SetActive(false);
+        popUp.SetActive(true);
+    }
+
+    public void ShowX2PowerUp() 
+    {
+        popUpManager.CheckPowerUp();
+        priceText.text = pricePowerUpTextValue;
+        X2Text.SetActive(true);
+        plus50MText.SetActive(false);
+        menuButton.SetActive(false);
+        shop.SetActive(false);
+        popUp.SetActive(true);
+    }
+
+    public void ShowPlusMetersPowerUp() 
+    {
+        popUpManager.CheckPowerUp();
+        priceText.text = pricePowerUpTextValue;
+        X2Text.SetActive(false);
+        plus50MText.SetActive(true);
+        menuButton.SetActive(false);
         shop.SetActive(false);
         popUp.SetActive(true);
     }
@@ -53,6 +89,14 @@ public class ShopMenuButtons : MonoBehaviour
     public void PopUpBuy() 
     {
         popUp.SetActive(false);
+        menuButton.SetActive(true);
+        shop.SetActive(true);
+    }
+
+    public void PopUpExit() 
+    {
+        popUp.SetActive(false);
+        menuButton.SetActive(true);
         shop.SetActive(true);
     }
 }
