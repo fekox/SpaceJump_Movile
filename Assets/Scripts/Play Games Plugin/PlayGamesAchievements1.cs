f3df2abc93037cf2e7b0dcb1a99ca51b17ec24e7
fekox <facusantos123@gmail.com>
2023-11-25 17:15:36 -0300

    Add PlayerGamesAchievements

---
+++ Assets/Scripts/Play Games Plugin/PlayGamesAchievements1.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames.BasicApi.SavedGame;
+using UnityEngine.SocialPlatforms;
+using System;
+    
+public class PlayGamesAchievements1 : MonoBehaviour
+{
+    public static PlayGamesAchievements1 instance;
+
+    static bool active = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ShowAchievements1() 
+    {
+#if UNITY_ANDROID
+        if (Social.localUser.authenticated) 
+        {
+            Social.ShowAchievementsUI();
+        }
+#endif
+    }
+
+    public void Get10Points() 
+    {
+#if UNITY_ANDROID
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportProgress("CgkI7aPZp4cCEAIQAQ", 100f, success => { });
+        }
+#endif
+    }
+
+    public void Get50Points()
+    {
+#if UNITY_ANDROID
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportProgress("CgkI7aPZp4cCEAIQAg", 100f, success => { });
+        }
+#endif
+    }
+
+    public void Get100Points()
+    {
+#if UNITY_ANDROID
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportProgress("CgkI7aPZp4cCEAIQAw", 100f, success => { });
+        }
+#endif
+    }
+}
